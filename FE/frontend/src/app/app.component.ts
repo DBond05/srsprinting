9d264ce0f61edf006b593655c78103a44d4d5551
DBond05 <dbond.tech@gmail.com>
2024-03-09 23:08:23 -0600

    under construction

--- FE/frontend/src/app/app.component.ts
+++ FE/frontend/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ContactComponent } from "./components/contact/contact.component";
+import { LandingComponent } from "./components/landing/landing.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavBarComponent, ContactComponent]
+    imports: [RouterOutlet, NavBarComponent, ContactComponent, LandingComponent]
 })
 export class AppComponent {
   title = 'SRS-Printing';
